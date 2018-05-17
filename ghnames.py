b3ffd719307aab50e95cdd13d6b82c1b510d915a
Elliott Hauser <eah13@mac.com>
2018-05-17 23:10:02 +0000

    Add gh usernames

--- ghnames.py
+++ ghnames.py
@@ -4,37 +4,25 @@
 
 # In Python """ starts a string that can span multiple lines
 # Add student github names here.
-names = """jbfelder
-zman7895
-chall12
-samsr31
-kmorbitzer
-grassycheetah94
-chausuble
-jpanken
-lisetted
-anusha-suresh
-nvola
-abaker8
-businessowl
-jmo560
-neatoskeeto
-alexreher
-WildGinger23
-Ibbi2010
-brianpugsley
-nurahill"""
+names = """scylarmartin
+ponderm
+TCarmon
+rachelanniexo
+kevin-kao
+hollaATyacodegirl
+draravindrk
+NobrineCalc"""
 
 
 # The data format our blog uses is called YAML.  It's a common way of
 # getting simple data into a program that processes text.
-for name in sorted(names.split("\n"), key=str.lower): # the .split() method makes a list out of a string, split at a character. The sorted() function alphabetizes them. 
-    print """  {0}:                    
+for name in sorted(names.split("\n"), key=str.lower): # the .split() method makes a list out of a string, split at a character. The sorted() function alphabetizes them.
+    print """  {0}:
     name: {0}
     prof: false
     gravatar:
-    website: 
+    website: "#"
     github: {0}
-    twitter: 
+    twitter:
     about: "Here's a little about {0}"
     """.format(name)   # The format method inserts the nth argument at {n} (starting with the zeroth)
